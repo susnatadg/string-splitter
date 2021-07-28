6ef4527d1d3fe71f0cfc8127cdb5ffcfdd8d62e5
unknown <susnatadg@gmail.com>
2021-07-28 16:25:15 +0100

    cut2 - initial push

--- test_tag_manipulator.py
+++ test_tag_manipulator.py
@@ -1,5 +1,5 @@
 from tag_manipulator import TagManipulator
-    
+
 def test_split_empty_string_result_empty_array():
         # arrange
     stringToSplit = ""
@@ -18,8 +18,7 @@ def test_split_comma_empty_string_result_empty_array():
     stringToSplit = ","
     expResult = []
     result = None
-    cut = TagManipulator()
-
+    cut =  TagManipulator()
         # act
     result = cut.parse_string(stringToSplit)
 

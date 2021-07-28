aac851e994c51295a776a22214e9b6ba35e550b8
unknown <susnatadg@gmail.com>
2021-07-28 16:37:38 +0100

    Commit for the java tests

--- tag_manipulator.py
+++ tag_manipulator.py
@@ -1,9 +1,23 @@
 class TagManipulator():    
     
+    #def parse_string(self, tags):
+        #result = []
+
+        #if len(tags) < 1 :
+            #return result
+
+        #preturn result
+
     def parse_string(self, tags):
-        result = []
 
-        if len(tags) < 1 :
-            return result
+         result = []
 
-        return result
+         if len(tags) < 1 or tags == ",":
+             return result
+
+         result = tags.split(",")
+         for index in range(0, len(result)):
+             new_entry = result[index].strip()
+             result[index] = new_entry
+
+         return result
